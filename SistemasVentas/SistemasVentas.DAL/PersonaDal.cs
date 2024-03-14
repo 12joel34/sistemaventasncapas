4ecacf8ddca628bd8d47e6f54a8266e4f0236300
12joel34 <joel123manguia@gmail.com>
2024-03-14 01:02:39 -0400

    proyecto

---
+++ SistemasVentas/SistemasVentas.DAL/PersonaDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace SistemasVentas.DAL
+{
+    public class PersonaDal
+    {
+        public DataTable ListarPersonasDal() 
+        {
+            string consulta = "select * from persona";
+            DataTable lista = conexion.EjecutarDataTabla(consulta, "tabla");
+            return lista;
+        }
+    }
+}
